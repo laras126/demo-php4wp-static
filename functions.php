62bdbfab1ec152b60baba5acfd61aa93cae02de3
laras126 <laras126@gmail.com>
2016-08-27 10:28:31 -0400

    add include files and comments

--- functions.php
+++ functions.php
@@ -1,5 +1,20 @@
 <?php
 
+/*
+ *
+ * Define some shopping lists
+ *
+ */
+
+// A single dimension array
+$clothes = array(
+            'shoes' => 'sneakers',
+            'top' => 't-shirt',
+            'bottom' => 'slacks',
+            'accessory' => 'watch'
+          );
+
+// A multidimensional array
 $groceries = array(
               'veggies' => array(
                   'beets',
@@ -11,26 +26,36 @@ $groceries = array(
               'meat' => 'lamb'
             );
 
-$clothes = array(
-            'shoes' => 'sneakers',
-            'top' => 't-shirt',
-            'bottom' => 'slacks',
-            'accessory' => 'watch'
-          );
-
-
+// A function to echo various shopping lists
 function goShopping($arr) {
+
+  // Loop through the array that has been passed to the function
   foreach ($arr as $type => $value) {
 
+    // Print the type, or key, of the array
     echo '<strong>' . $type . ':</strong> ';
 
     if ( is_array($value) ) {
-      foreach ($value as $val) {
+
+      // If the value is an array, loop through and echo it.
+      // Alternative syntax with colon and endforeach.
+      foreach ($value as $val):
+
+        // Now, how not have after the last one in the list?
+        // There are many ways, and I don't feel like figuring it out. WordPress usually handles this.
+        // http://stackoverflow.com/questions/2435216/how-to-create-comma-separated-list-from-array-in-php
         echo $val . ', ';
-      }
+
+      endforeach;
+
     } else {
+
+      // Otherwise, just print the single value.
       echo $value;
+
     }
+    // Create a break between each list item
     echo '<br>';
   }
 }
+
--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <?php
 
+// Include the file with our functionality to keep our markup cleaner.
 require('functions.php');
 
+// Include the markup for the header
+require('header.php');
+
+// Print out various lists
 goShopping($groceries);
 goShopping($clothes);
 
+// Print the array object
 echo '<pre>';
 print_r($groceries);
 echo '</pre>';
+
+// Include the markup for the footer
+require('footer.php');
